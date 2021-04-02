edac930a8468f548b8f08809e41f5953e0c55302
mohamedibrahim155 <ibrahim155.developer@gmail.com>
2021-04-02 16:27:36 +0530

    SetUp

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    Rigidbody rb;
+    public float ballSpeed;
+
+   
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+  
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        rb.AddTorque(new Vector3(horizontal, 0, vertical) * ballSpeed);
+    }
+}
