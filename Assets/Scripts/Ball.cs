b7165f4766f5b80b363bbd3191ad69955530a761
Uphendra <altairuphi0523@gmail.com>
2021-04-02 20:21:43 +0530

    URP Initialised

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,13 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Ball : MonoBehaviour
 {
     Rigidbody rb;
     public float ballSpeed;
+    public Image bar;
+    public float reduceAmount, lerpTime;
+    public bool IsHotSpot;
+    public Color[] colors;
 
-   
     void Start()
     {
         rb = GetComponent<Rigidbody>();
@@ -16,9 +20,63 @@ public class Ball : MonoBehaviour
   
     void Update()
     {
+        Mathf.Clamp(bar.fillAmount, 0, 1);
+
+
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
+        rb.AddTorque(new Vector3(horizontal, 0, vertical) * ballSpeed );
 
-        rb.AddTorque(new Vector3(horizontal, 0, vertical) * ballSpeed);
+        if(IsHotSpot == true)
+        {
+            IncreaseHeatBar();
+        }
+        else
+        {
+            ReduceHeatBar();
+        }
+
+        if(bar.fillAmount >=1)
+        Debug.Log("OVERHEATED");
+
+     
+
+
     }
+
+
+    public void ReduceHeatBar()
+    {
+        bar.fillAmount -= Time.deltaTime * reduceAmount;
+       // bar.color = Color.Lerp(bar.color, lowBar, lerpTime*Time.deltaTime);
+
+    }
+
+    public void IncreaseHeatBar()
+    {
+        bar.fillAmount += Time.deltaTime * reduceAmount;
+       // bar.color = Color.Lerp(bar.color, fullBar, lerpTime*Time.deltaTime);
+    }
+
+
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Hotspot"))
+        {
+            IsHotSpot = true;
+        }
+       
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject.CompareTag("Hotspot"))
+        {
+            IsHotSpot = false;
+
+        }
+    }
+
 }
---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+
+    public Vector3 offset;
+
+    public float smoothSpeed;
+    
+    public void LateUpdate()
+    {
+        Vector3 desiredPos = target.position + offset;
+        Vector3 smoothedPos = Vector3.Lerp(transform.position, desiredPos, smoothSpeed);
+        transform.position = smoothedPos;
+
+        //transform.position = desiredPos;
+            transform.LookAt(target);
+        
+    }
+
+
+    
+}
